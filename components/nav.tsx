c2fb02fb7f21cb912476eff2186f348a4888eebf
Altismann <87611738+Altismann@users.noreply.github.com>
2025-01-13 12:09:06 +0000

    frenkld

--- components/nav.tsx
+++ components/nav.tsx
@@ -8,7 +8,7 @@ export type NavType = {
 const Nav: NextPage<NavType> = ({ className = "" }) => {
   return (
     <header
-      className={`self-stretch bg-black flex flex-row items-center justify-center py-2.5 z-[99] text-left text-sm text-white font-montserrat ${className}`}
+      className={`w-full bg-black flex flex-row items-center justify-center py-2.5 z-[99] text-left text-sm text-white font-montserrat ${className}`}
     >
       <Image
         className="h-10 w-10 relative overflow-hidden shrink-0"
--- pages/index.tsx
+++ pages/index.tsx
@@ -5,14 +5,14 @@ const Body: NextPage = () => {
   return (
   <div className=" bg-black" >
   <Nav />
-  <div className="absolute z-30 text-45xl uppercase font-montserrat text-white text-opacity-80 font-black items-center justify-center mq450:mt-[75px] mq750:mt-[150px] mt-[300px] mq450:text-[24px] mq750:text-[36px] mq450:leading-[24px] mq750:leading-[36px]" >
+  <div className="flex absolute z-30 text-[90px] uppercase font-montserrat text-white text-opacity-80 font-black mq450:text-[24px] mt-40 mq750:text-[36px] mq450:leading-[24px] mq750:leading-[36px]" >
     Portfolio för arbetsgivare
   </div>
   <video
     autoPlay
     loop
     muted
-    className="relative z-10 min-h-30% max-w-full overflow-hidden"
+    className="w-full relative z-10 min-h-30% max-w-full"
   >
     <source
       src="https://cdn.pixabay.com/video/2021/07/08/80711-573496150_large.mp4"
@@ -22,17 +22,17 @@ const Body: NextPage = () => {
   </video>
 
       
-      <main className="w-full flex flex-col items-center justify-center box-border relative gap-[227px] max-w-full text-left text-27xl text-white font-montserrat ">
-      <section className="w-full leading-[60px] uppercase font-black font-montserrat text-white text- flex items-center mq450:text-[18px] mq750:text-[36px] mq450:leading-[18px] mq750:leading-[36px]">
+      <main className="w-full flex flex-col items-center justify-center box-border relative max-w-full text-left text-27xl text-white font-montserrat ">
+      <section className="w-full leading-[60px] uppercase font-black font-montserrat text-white items-center mq450:text-[24px] mq750:text-[36px] mq450:leading-[24px] mq750:leading-[36px]">
         Hej! Jag heter Anders, jag är en hobby produktdesigner och 
         webbutvecklare plaserad i Stockholm. Alla sidor här är skapade från
         grunden utan färdiga teman. Jag använde Figma, Locofy och Nextjs för 
         att göra den här sidan.
         </section>
-        <div className="w-full flex flex-col items-center justify-center ">
+        <div className="w-full flex flex-col items-center justify-center mt-[85px] ">
           <div className="flex flex-row items-center justify-center">
             <h2 className="relative leading-[64px] font-black font-[inherit] flex items-center mq450:text-[24px] mq750:text-[36px] mq450:leading-[24px] mq750:leading-[36px]">
-              <section className="w-full leading-[60px] uppercase font-black mb-[150px] font-montserrat text-white items-center mq450:text-[18px] mq750:text-[36px] mq450:leading-[18px] mq750:leading-[36px]">
+              <section className="w-full leading-[60px] uppercase font-black font-montserrat text-white items-center mq450:text-[18px] mq750:text-[36px] mq450:leading-[18px] mq750:leading-[36px]">
               Web Developer
                 (Mimo)
                 Certifiering i JS
@@ -42,34 +42,28 @@ const Body: NextPage = () => {
             </h2>
 
           </div>
-          <div className="flex flex-col items-center justify-center py-0 px-[26px] box-border max-w-full text-center">
-            <div className="flex-1 flex flex-col items-end justify-center relative mq450:gap-20 max-w-full">
-              <h2 className="flex flex-col relative text-45xl breal-all leading-[64px] lowercase font-black font-[inherit] items-center justify-center ">
-                Den här sidan
+          <div className="flex flex-col mt-[75px] max-w-full">
+            <div className="max-w-full">
+              <h2 className="flex flex-col relative text-[74px] break-all leading-[74px] lowercase font-black items-center justify-center ">
+                Mitt hobby projekt:
               </h2>
               
               
-              <div className="flex flex-col items-start justify-center mt-1 max-w-full [row-gap:20px] ">
+              <div className="flex flex-col max-w-full ">
               <a
-          className="relative [text-decoration:underline] text-45xl font-black leading-[40px] text-pink-400 font-bold text-[inherit]"
+          className="relative [text-decoration:underline] mt-[-65px] text-[74px] font-black break-all leading-[74px] text-pink-400 "
           href="https://www.dyoid.se"
           target="_blank"
         >
-          dyoid
+          bydyoid
         </a>
-                <div className="relative text-5xl leading-[20px] lowercase font-black inline-block min-w-[120px] ">
-                  Webflow
-                </div>
-              </div>
-              <div className="flex-col absolute break-all text-5xl leading-[64px] lowercase font-black flex items-center justify-center min-w-[85px] ">
-                Nextjs
               </div>
             </div>
           </div>
         </div>
 
-        <div className="w-[440px] flex flex-row items-start justify-center pt-0 box-border max-w-full text-center ">
-          <div className="h-[336px] relative leading-[48px] font-black flex items-center ">
+        <div className="w-[440px] flex flex-row items-start justify-center mt-[300px] box-border max-w-full text-center ">
+          <div className="h-[336px] relative leading-[48px] font-black flex break-all items-center ">
             <span className="[line-break:none]">
               <p className="m-0">Vscode</p>
               <p className="m-0 tracking-[25px]">NextJs</p>
@@ -82,8 +76,8 @@ const Body: NextPage = () => {
             </span>
           </div>
         </div>
-        <div className="w-full flex flex-col items-center justify-center py-0 px-[0px] box-border max-w-full text-center pb-[200px]">
-          <div className="flex-1 flex flex-col items-center justify-center gap-3.5 max-w-full">
+        <div className="w-full flex flex-col items-center justify-center mt-[300px] box-border max-w-full text-center pb-[200px]">
+          <div className="flex-1 flex flex-col items-center justify-center max-w-full">
 
             <div className="w-full text-17xl leading-[36px] font-black ">
               <h2 className="m-0 mb-1">Kontakt</h2>
